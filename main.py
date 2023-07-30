a9bfa791f7a3d8c808aa1166d0c75b51dde269ab
Riley-26 <107146846+Riley-26@users.noreply.github.com>
2023-07-30 10:50:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Player:
         old_location = old_location.upper()
         new_location = new_location.upper()
         try:
-            if old_location in board_dict and board_dict[old_location] != "   0":
+            if old_location in board_dict:
                 chosen_piece = board_dict[old_location]
                 #Check if piece is in way
                 if chosen_piece.valid_move(old_location, new_location):
@@ -116,25 +116,21 @@ class Board:
         return ""
     
     def update_board(self, old_location, new_location):
-        #Update the new location in the board dictionary
-        if board_dict[new_location] == "   0":
-            board_dict[new_location] = board_dict[old_location]
-            board_dict[old_location] = "   0"
-        elif board_dict[new_location].short_name != board_dict[old_location].short_name:
-            board_dict[new_location] = board_dict[old_location]
-            board_dict[old_location] = "   0"
-        
+        print(board_dict)
         #Update the new location in the displayed board
         for x, item in enumerate(board.grid):
             for y in range(len(item)):
                 if (8 - int(new_location[1])) == x:
                     if char_codes[new_location[0].lower()] - 1 == y:
-                        print(x, y)
-                        board.grid[x][y] = "[" + str(new_location) + ": " + str(board_dict[new_location].short_name) + "]"
+                        board.grid[x][y] = "[" + str(new_location) + ": " + str(board_dict[old_location].short_name) + "]"
                 elif (8 - int(old_location[1])) == x:
                     if char_codes[old_location[0].lower()] - 1 == y:
                         board.grid[x][y] = "[" + str(old_location) + ": " + "   0" + "]"
-
+                        
+        #Update the new location in the board dictionary
+        board_dict[new_location] = board_dict[old_location]
+        board_dict[old_location] = "   0"
+            
         return ""
 
     
@@ -142,16 +138,18 @@ class Board:
 class Piece:
     def __init__(self, colour, piece_name):
         self.colour = colour
-        self.taken = False
+        self.piece_taken = False
         self.clear_move = False
         self.piece_name = piece_name
     
-    def taken(self):
+    def taken(self, location):
         if self.taken == True:
-            if self.colour == black:
-                del valid_dict.black_pieces[self.piece_name]
-            elif self.colour == white:
-                del valid_dict.white_pieces[self.piece_name]
+            if self.colour == "black":
+                del board_dict[location]
+            elif self.colour == "white":
+                del board_dict[location]
+        
+        return self.colour + board_dict[location].piece_name + " has been taken"
 
 
 #Each piece
@@ -166,33 +164,29 @@ class Pawn(Piece):
         valid = False
         #Checks if the input is valid
         if board_dict[old_location].colour == player_names[current_move].colour:
-            print(1)
             if len(new_location) == 2 and len(old_location) == 2:
-                print(2)
                 if new_location[0].lower() in chars and old_location[0].lower() in chars:
-                    print(3)
                     if int(new_location[1]) in char_indices and int(old_location[1]) in char_indices:
-                        print(4)
                         valid = True
                     else:
-                        print(5)
                         valid = False
                         return valid
                 else:
-                    print(6)
                     valid = False
                     return valid
             else:
-                print(7)
                 valid = False
                 return valid
         else:
-            print(8)
             valid = False
             return valid
         
         #Check if there is a piece in the way
-        if board_dict.get(new_location) != "   0":
+        if board_dict[new_location] == "   0":
+            valid = True
+        elif board_dict[new_location].colour[0] != self.colour[0]:
+            valid = True
+        else:
             valid = False
             return valid
         
@@ -207,17 +201,17 @@ class Pawn(Piece):
                 return valid
             
             #Check if a piece can be taken
-            print(1)
             if new_location_num == old_location_num - 1:
-                print(2)
+                print(0)
                 if (int(char_codes.get(new_location[0].lower())) == int(char_codes.get(old_location[0].lower())) + 1) or (int(char_codes.get(new_location[0].lower())) == int(char_codes.get(old_location[0].lower())) - 1):
-                    print(4)
-                    if board_dict[new_location] != "   0" and board_dict[new_location].short_name == "w-":
-                        board_dict[new_location].taken = True
-                        board_dict[new_location].taken()
-                        board.update_board(old_location, new_location)
+                    print(1)
+                    if board_dict[new_location] != "   0" and board_dict[new_location].colour[0] != self.colour[0]:
+                        print(2)
+                        board_dict[new_location].piece_taken = True
+                        board_dict[new_location].taken(old_location)
                         valid = True
                     else:
+                        print(3)
                         valid = False
                     
                     return valid
